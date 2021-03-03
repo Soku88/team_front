ad036ee0ae5326b279b605fa39010324e2840239
Soku88 <essence.soku@gmail.com>
2021-03-03 16:22:34 +0900

    add detail page review modal

--- soku/js/main.js
+++ soku/js/main.js
@@ -2,6 +2,7 @@ const modal = document.querySelector(".modal-wrapper");
 const mapModal = document.querySelector(".map-modal");
 const mapModalImg = document.querySelector(".map-modal-img");
 const menu = document.querySelector(".menu");
+const review = document.querySelector(".review-wrap");
 
 // profile toggle
 function toggleMenu() {
@@ -19,9 +20,16 @@ window.addEventListener("click", function (e) {
 //map-modal
 function mapModalHandle() {
 	mapModal.classList.toggle("map-modal__open");
-	console.log("modal");
 }
 window.addEventListener("click", function (e) {
 	e.target === mapModalImg ? mapModal.classList.remove("map-modal__open") : false;
-	console.log(e.target);
+});
+
+//review-modal
+function reviewModalHandle() {
+	review.classList.toggle("review-open");
+}
+
+window.addEventListener("click", function (e) {
+	e.target === review ? review.classList.remove("review-open") : false;
 });
